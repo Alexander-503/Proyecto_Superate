52fe0d208c105b3314d8603a60b4b78e1eac15aa
Alexander Vasquez <eduardo.vasquezm15@gmail.com>
2022-11-07 22:32:23 -0600

    :pencil2: Update word

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Crear cuenta') }}</div>
+                <div class="card-header">{{ __('Crear Cuenta') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
